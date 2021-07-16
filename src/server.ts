da7f5fe3132c4c66e98aa7284b954485e34f2354
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-07-16 19:21:21 -0300

    feat: Page not found and static folder

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 import mainRoutes from './routes/index';
 
 const server = express();
 
+server.use(express.static(path.join(__dirname, '../public')));
+
 server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Página não encontrada!');
+});
 
 server.listen(3000, () => {
   console.log('Servidor rodando na porta:', 3000);
